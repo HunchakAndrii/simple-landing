9c20de17bdd5578382f6f47d08ce93fd6732aedd
HunchakAndrii <andreygunchak1@gmail.com>
2024-02-17 20:37:06 +0200

    make slider with adaptive

--- js/main.js
+++ js/main.js
@@ -15,3 +15,37 @@ if (burger) {
     }
   });
 }
+
+
+// slider //
+
+const swiper = new Swiper(".swiper", {
+  // Optional parameters
+  direction: "horizontal",
+  loop: true,
+  slidesPerView: 1,
+  spaceBetween: 20,
+
+  // Navigation arrows
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
+  },
+
+  breakpoints: {
+    800: {
+      slidesPerView: 2,
+      spaceBetween: 20,
+    },
+    900: {
+      slidesPerView: 3,
+      spaceBetween: 50,
+    },
+  },
+  autoplay: {
+    delay: 2000, // 
+    disableOnInteraction: false, 
+  },
+});
+
+
